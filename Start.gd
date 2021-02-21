db737d4bf9050af6463b1a2aee3c57cf3e12163c
Olaf Horbowy <olafhorbowy@gmail.com>
2021-02-21 20:28:13 +0100

    Przyklad unix time

--- Start.gd
+++ Start.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var data_dictionary = {}
+var date
 
 
 func _ready():
@@ -8,7 +8,8 @@ func _ready():
 
 
 func _on_StartButton_pressed():
-	var date = OS.get_date()
-	data_dictionary["New_date"] = date
+	var date = OS.get_unix_time();
 	get_tree().change_scene("res://Wybierz.tscn")
 	print(date)
+	OS.delay_msec(5000)
+	print(OS.get_unix_time() - date);
